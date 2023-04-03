77ccfb16302e8dc85c3125d76662508921d06865
maartenvanderbeeken <maartenvanderbeeken@hotmail.com>
2023-04-03 14:55:39 +0100

    Task 1 of cw2 completed

--- seed.php
+++ seed.php
@@ -1,90 +1,31 @@
 <?php
 
-    include("_includes/config.inc");
-    include("_includes/dbconnect.inc");
+    //include("_includes/config.inc");
+    //include("_includes/dbconnect.inc");
     include("_includes/functions.inc");
 
+    require_once '/Users/maartenvanderbeeken/vendor/autoload.php';
 
-    // Checks whether we are logged in
-    if (isset($_SESSION['id'])) {
+    $faker = Faker\Factory::create();
 
-        // $array_students = array(
-        //     array(
-        //         "studentid" => "21424839",
-        //         "password" => "test",
-        //         "dob" => "23121994",
-        //         "firstname" => "Maarten",
-        //         "lastname" => "Vanderbeeken",
-        //         "house" => "Stanton house",
-        //         "town" => "Aylesbury",
-        //         "county" => "Buckinghamshire",
-        //         "country" => "England",
-        //         "postcode" => "hp218fq"
-        //     ),
-        //     array(
-        //         "studentid" => "21743499",
-        //         "password" => "bop",
-        //         "dob" => "14071999",
-        //         "firstname" => "James",
-        //         "lastname" => "Day",
-        //         "house" => "Apple house",
-        //         "town" => "Aylesbury",
-        //         "county" => "Buckinghamshire",
-        //         "country" => "England",
-        //         "postcode" => "hp225dz"
-        //     ),
-        //     array(
-        //         "studentid" => "22477347",
-        //         "password" => "mep",
-        //         "dob" => "01092001",
-        //         "firstname" => "Kevin",
-        //         "lastname" => "Jones",
-        //         "house" => "Osmand house",
-        //         "town" => "Tring",
-        //         "county" => "Buckinghamshire",
-        //         "country" => "England",
-        //         "postcode" => "hp446ez"
-        //     ),
-        //     array(
-        //         "studentid" => "22443388",
-        //         "password" => "bam",
-        //         "dob" => "22092002",
-        //         "firstname" => "Mary",
-        //         "lastname" => "Smith",
-        //         "house" => "Summer's house",
-        //         "town" => "High Wycombe",
-        //         "county" => "Buckinghamshire",
-        //         "country" => "England",
-        //         "postcode" => "hp887fx"
-        //     ),
-        //     array(
-        //         "studentid" => "22472144",
-        //         "password" => "elm",
-        //         "dob" => "31012001",
-        //         "firstname" => "Sarah",
-        //         "lastname" => "Fowle",
-        //         "house" => "Burberry house",
-        //         "town" => "Marlow",
-        //         "county" => "Buckinghamshire",
-        //         "country" => "England",
-        //         "postcode" => "hp456gh"
-        //     ),
-        // );
-        
-        // foreach ($array_students as $key => $student_array) {
-        //     $student_array
-        // }
+    $db = new mysqli("localhost", "root", "", "cw2_students");
 
-        $sql = "INSERT INTO student (studentid, password, dob, firstname, 
-                lastname, house, town, county, country, postcode)
-                VALUES (value1, value2, value3, value4, value5, value6, 
-                value7, value8, value9, value10)";
+    $db->query("DELETE FROM student");
+    
+    $db->query("ALTER TABLE student AUTO_INCREMENT = 1");
 
-        $result = mysqli_query($conn,$sql);
+    foreach(range(1,30) as $x) {
+        $student_id = $x;
 
-        // INSERT INTO table_name (column1, column2, column3, ...)
-        // VALUES (value1, value2, value3, ...);
+        $db->query("
+            INSERT INTO student (studentid, password, dob, firstname, 
+            lastname, house, town, county, country, postcode)
+            VALUES ('$student_id', '{$faker->password}', 
+            '{$faker->date}', '{$faker->firstName}', '{$faker->lastName}', '{$faker->word}', 
+            '{$faker->city}', '{$faker->city}', '{$faker->country}', '{$faker->postcode}')
+        ");
+    }
 
-    } 
+
 
 ?>
